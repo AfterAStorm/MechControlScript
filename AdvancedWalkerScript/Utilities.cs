cedb09cc1b677438b5811dabb56f8d52e17d48f8
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2024-01-12 16:32:22 -0600

    Add utilities (angle conversions) extension class

---
+++ AdvancedWalkerScript/Utilities.cs
@@ -0,0 +1,57 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    public static class AngleConversions
+    {
+        /// <summary>
+        /// Converts radians to degrees
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static double ToDegrees(this double radians)
+        {
+            return MathHelper.ToDegrees(radians);
+        }
+
+        /// <summary>
+        /// Converts degrees to radians
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static double ToRadians(this double degrees)
+        {
+            return MathHelper.ToRadians(degrees);
+        }
+
+        /// <summary>
+        /// Turns a -180 to 180 (since rotors operate between 0 and 360)
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        public static double AbsoluteDegrees(this double degrees)
+        {
+            while (degrees < 0)
+                degrees += 360;
+            return degrees;
+        }
+    }
+}
