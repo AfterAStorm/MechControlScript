324280911d94e012fcaa1ec56cbe971eb0430397
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2024-01-13 17:51:20 -0600

    Configuration changes, average ms runtime, new block fetching, new leg cycle

--- AdvancedWalkerScript/Program.cs
+++ AdvancedWalkerScript/Program.cs
@@ -1,4 +1,5 @@
-﻿using Sandbox.Game.EntityComponents;
+﻿using Sandbox.Game.AI.Commands;
+using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using SpaceEngineers.Game.ModAPI.Ingame;
@@ -6,6 +7,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Text;
 using VRage;
@@ -44,24 +46,17 @@ namespace IngameScript
          * 2 = Humanoid
          * 3 = Spideroid
          * 4 = Digitigrade
-         * 
         */
-        byte LegType = 1;
 
-        bool InvertHips = false; // invert hips?
-        bool InvertKnees = false; // invert knees?
-        bool InvertFeet = false; // invert feet?
-
-        float HipOffset = 0f; // degrees, applies to ALL hips
-        float KneeOffset = 0f; // degrees, applies to ALL knees
-        float FootOffset = 0f; // degrees, applies to ALL feet
+        static float AccelerationMultiplier = 1f; // how fast the mech accelerates, 1f is normal, .5f is half speed, 2f is double speed
+        static float DecelerationMultiplier = 1; //  how fast the mech decelerates, same as above
 
         static float MaxRPM = float.MaxValue; // 60f is the max speed for rotors
         // *Configure motor limits in the blocks themselves!* //
 
         // - Walking
 
-        static float WalkSpeed = 2f;
+        static float WalkCycleSpeed = 2f;
 
         // -- Debug -- \\
 
@@ -78,23 +73,29 @@ namespace IngameScript
 
         //private MyIni configuration = new MyIni();
 
+        // Default Joint Configuration //
+
+        // Diagnostics //
+
+        double[] averageRuntimes = new double[20];
+        int averageRuntimeIndex = 0;
+
         // Variables //
 
         public static IMyTextPanel debug = null;
 
-        IMyShipController cockpit = null;
-        IMyShipController reference = null;
+        List<IMyShipController> cockpits = new List<IMyShipController>();
         private List<LegGroup> legs = new List<LegGroup>();
         private bool crouched = false;
         private bool crouchOverride = false; // argument crouch
 
         Vector3 movement = Vector3.Zero;
 
-        private new void Echo(params string[] messages)
+        private static void Log(params string[] messages)
         {
             string message = string.Join(" ", messages);
             if (debug == null)
-                base.Echo(message);
+                LegGroup.Program.Echo(message);
             else
                 debug.WriteText(message + "\n", true);
         }
@@ -111,6 +112,107 @@ namespace IngameScript
         }
 
         /// <summary>
+        /// This checks a stator and sees if it should add it to the lists of a <see cref="LegGroup"/>
+        /// <br />
+        /// It, Is, NIGHTMARE FUEL
+        /// </summary>
+        /// <param name="stator"></param>
+        /// <param name="pattern"></param>
+        /// <param name="lastLegs"></param>
+        /// <param name="currentLegs"></param>
+        /// <param name="createLegAnyway"></param>
+        /// <returns></returns>
+        private bool CheckStator(IMyMotorStator stator, System.Text.RegularExpressions.Regex pattern, ref List<LegGroup> lastLegs, ref List<LegGroup> currentLegs, bool createLegAnyway = false)
+        {
+            foreach (string segment in stator.CustomName.Split(' '))
+            {
+                System.Text.RegularExpressions.Match match = pattern.Match(segment.ToLower());
+                if (!match.Success || match.Groups[1].Value.Length <= 0 || match.Groups[2].Value.Length <= 0)
+                    continue;
+                int id;
+                bool parsedId = int.TryParse(match.Groups[3].Value, out id);
+                LegGroup lastLeg = lastLegs.IsValidIndex(id) ? lastLegs[id] : null;
+                LegConfiguration? lastConfig = lastLeg?.Configuration;
+                LegGroup currentLeg = currentLegs.IsValidIndex(id) ? currentLegs[id] : null;
+
+                if (currentLeg == null)
+                {
+                    if (lastConfig.HasValue && lastConfig.Value.HasChanged(stator.CustomData))
+                    {
+                        LegConfiguration? configuration = LegConfiguration.Parse(stator.CustomData);
+                        if (configuration.HasValue)
+                        {
+                            currentLeg = CreateLegFromType(configuration.Value.LegType);
+                            currentLeg.Configuration = configuration.Value;
+                            currentLegs.Insert(id, currentLeg);
+                        }
+                    }
+                    if (currentLeg == null && createLegAnyway)
+                    {
+                        LegConfiguration configuration = LegConfiguration.DEFAULT;
+                        currentLeg = CreateLegFromType(configuration.LegType);
+                        currentLeg.Configuration = configuration;
+                        currentLegs.Insert(id, currentLeg);
+                    }
+                    else if (currentLeg == null)
+                        return true; // check later, there is a possibility of another joint having a valid config
+                }
+
+                currentLeg.Configuration.AnimationSpeed = WalkCycleSpeed;
+                stator.CustomData = currentLeg.Configuration.ToCustomDataString();
+
+                bool isLeft = false;
+                switch (match.Groups[2].Value)
+                {
+                    case "l":
+                    case "left":
+                        isLeft = true;
+                        break;
+                    // no need to check right since it's infered
+                    // and the regex only searched for l, left, r, and right; so it can only ever be r and right anyway
+                }
+
+                Joint joint = new Joint(stator, new JointConfiguration()
+                {
+                    Inversed = match.Groups[4].Value.Equals("-"),
+                    Offset = 0
+                });
+
+                switch (match.Groups[1].Value)
+                {
+                    case "h":
+                    //case "hip":
+                        if (isLeft)
+                            currentLeg.LeftHipStators.Add(joint);
+                        else
+                            currentLeg.RightHipStators.Add(joint);
+                        break;
+                    case "k":
+                    //case "knee":
+                        if (isLeft)
+                            currentLeg.LeftKneeStators.Add(joint);
+                        else
+                            currentLeg.RightKneeStators.Add(joint);
+                        break;
+                    case "f":
+                    //case "fp":
+                    //case "foot":
+                    //case "feet":
+                        if (isLeft)
+                            currentLeg.LeftFootStators.Add(joint);
+                        else
+                            currentLeg.RightFootStators.Add(joint);
+                        break;
+                    default:
+                        Log($"Unknown joint type \"{match.Groups[0].Value}\"");
+                        break;
+                }
+                break;
+            }
+            return false; // do not check, it's good!
+        }
+
+        /// <summary>
         /// Gets the blocks required for operation
         /// Ran at startup and on request
         /// </summary>
@@ -123,30 +225,46 @@ namespace IngameScript
 
             string cockpitSpecifier = CockpitName;//GetConfigurationValue("Mech Core", "Cockpit").ToString();
             string referenceSpecifier = ReferenceName;//GetConfigurationValue("Mech Core", "Reference").ToString();
-            Echo("Looking for cockpit with specifier", cockpitSpecifier);
-            Echo("Looking for reference rc with specifier", referenceSpecifier);
+            Log("Looking for cockpit with specifier", cockpitSpecifier);
+            Log("Looking for reference rc with specifier", referenceSpecifier);
 
-            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-            GridTerminalSystem.GetBlocksOfType<IMyShipController>(blocks);
-            foreach (IMyShipController controller in blocks) // Two for one, less effecient but it's not run that often
+            // Get all cockpits if they are the main cockpit (or the main remote control)
+            GridTerminalSystem.GetBlocksOfType(cockpits, (controller) => (controller is IMyRemoteControl) ? (controller as IMyRemoteControl).GetProperty("MainRemoteControl").AsBool().GetValue(controller) : controller.IsMainCockpit);
+
+            Log($"{(cockpits.Count > 0 ? "Found" : "Didn't Find")} cockpit(s)");
+
+            List<IMyMotorStator> stators = new List<IMyMotorStator>();
+            GridTerminalSystem.GetBlocksOfType(stators);
+
+            List<LegGroup> lastLegs = legs;
+            List<LegGroup> currentLegs = new List<LegGroup>();
+            legs.Clear();
+
+            // Regex: HR5+ turns into [h, r, 5, +], HL turns into [h, l, null, null], HLeft5+ turns into [h, left, 5, +]
+            System.Text.RegularExpressions.Regex pattern = new System.Text.RegularExpressions.Regex(@"^([^lLrR]*)([lr]{1}|left{1}|right{1})([0-9]+)?([-+]{1})?$");
+
+            List<IMyMotorStator> recheckLater = new List<IMyMotorStator>();
+            foreach (IMyMotorStator stator in stators)
             {
-                if (cockpit == null && ((cockpitSpecifier.Equals("auto") && controller is IMyCockpit && controller.IsMainCockpit) || controller.CustomName.Equals(cockpitSpecifier)))
-                    cockpit = controller; // Main Remote Control isn't a property, so you have to get it manually AAAAAAAAAAAAAAAA
-                else if (reference == null && ((referenceSpecifier.Equals("auto") && controller is IMyRemoteControl && controller.GetProperty("MainRemoteControl").AsBool().GetValue(controller)) || controller.CustomName.Equals(referenceSpecifier)))
-                    reference = controller;
+                Log($"Checking stator {stator.CustomName}");
+                if (CheckStator(stator, pattern, ref lastLegs, ref currentLegs))
+                {
+                    recheckLater.Add(stator);
+                    Log($"Checking {stator.CustomName} later");
+                }
+                else
+                    Log($"{stator.CustomName} added to leg");
             }
 
-            // Debug
-            if (cockpit == null)
-                Echo("No cockpit found!");
-            else
-                Echo("Found cockpit", cockpit.CustomName);
-            if (reference == null)
-                Echo("No reference RC found!");
-            else
-                Echo("Found reference", reference.CustomName);
+            foreach (IMyMotorStator stator in recheckLater)
+            {
+                Log($"Rechecking stator {stator.CustomName}");
+                CheckStator(stator, pattern, ref lastLegs, ref currentLegs, true);
+            }
 
-            // Legs
+            legs = currentLegs;
+
+            /*// Legs
             legs.Clear();
             List<IMyMotorStator> motors = new List<IMyMotorStator>();
             for (int i = 0; i < LegGroups.Length; i++)
@@ -199,7 +317,7 @@ namespace IngameScript
                 leg.Offset = offset * 4;
 
                 legs.Add(leg);
-            }
+            }*/
         }
 
         /*private MyIniValue GetConfigurationValue(string section, string key)
@@ -252,7 +370,7 @@ namespace IngameScript
             //LoadConfiguration();
 
             // Initialize subclasses
-            Leg.Program = this;
+            LegGroup.Program = this;
 
             // Get blocks
             GetBlocks();
@@ -276,6 +394,19 @@ namespace IngameScript
         /// <param name="updateSource"></param>
         public void Main(string argument, UpdateType updateSource)
         {
+            // Diagnostics
+            double lastRuntime = Runtime.LastRunTimeMs;
+
+            averageRuntimes[averageRuntimeIndex] = lastRuntime;
+            averageRuntimeIndex = (averageRuntimeIndex + 1) % averageRuntimes.Length;
+
+            // Detailed Info
+            Echo("Advanced Walker Script");
+            Echo($"{legs.Count} leg{(legs.Count != 1 ? "s" : "")}");
+            Echo($"");
+            Echo($"Last Tick: {lastRuntime}ms");
+            Echo($"Average Tick: {averageRuntimes.Sum() / averageRuntimes.Length:.03}ms over {averageRuntimes.Length} samples");
+
             // Handle arguments
             if (argument != null)
                 switch (argument.ToLower().Trim()) // Clean up argument, allow inputs
@@ -283,7 +414,7 @@ namespace IngameScript
                     case "reload": // Reloads the script's blocks and configuration
                         GetBlocks();
                         break;
-                    case "crouch": // Toggle crouch
+                    case "crouch": // Toggle crouch (overrides the cockpit [c])
                         crouchOverride = !crouchOverride; // crouchOverride is for this specifically, because the normal crouched variable is set based on
                         // the MoveIndicator (then gets set to this value if true)
                         break;
@@ -293,7 +424,29 @@ namespace IngameScript
             if (!updateSource.HasFlag(UpdateType.Update1))
                 return;
 
-            if (cockpit == null || reference == null || legs.Count <= 0)
+            double delta = Runtime.TimeSinceLastRun.TotalSeconds;
+
+            IMyShipController controller = cockpits.Find((pit) => pit.IsUnderControl);
+
+            Vector3 moveInput = controller?.MoveIndicator ?? Vector3.Zero;
+
+            debug?.WriteText(""); // clear
+            Log("MAIN LOOP");
+            Log(moveInput.ToString());
+
+            crouched = moveInput.Y < 0 || crouchOverride;
+            // TODO: use crouched / crouching
+
+            Vector3 movementDirection = (moveInput - movement) * .5f;
+
+            movement.X += movementDirection.X * (movementDirection.X > 0 ? AccelerationMultiplier : DecelerationMultiplier);
+            movement.Z += movementDirection.Z * (movementDirection.X > 0 ? AccelerationMultiplier : DecelerationMultiplier);
+
+
+
+            foreach (LegGroup leg in legs)
+                leg.Update(delta);
+            /*if (cockpit == null || reference == null || legs.Count <= 0)
                 return; // Not ready
 
             double delta = Runtime.TimeSinceLastRun.TotalSeconds;
@@ -326,14 +479,7 @@ namespace IngameScript
                 delta = 0;
             }
             foreach (Leg leg in legs)
-                leg.Update(delta);
-
-            // Update echo
-            double lastRuntime = Runtime.LastRunTimeMs;
-            base.Echo("Advanced Walker Script");
-            base.Echo($"{legs.Count} leg{(legs.Count != 1 ? "s" : "")}");
-            base.Echo($"");
-            base.Echo($"Last Tick: {lastRuntime}ms");
+                leg.Update(delta);*/
         }
     }
 }
