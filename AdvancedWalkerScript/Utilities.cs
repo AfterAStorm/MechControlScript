2f90ccaf9c9c550695493c5a4fb8e9bf60820c88
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2024-01-13 17:50:49 -0600

    Tweaked absolute degrees for hinges

--- AdvancedWalkerScript/Utilities.cs
+++ AdvancedWalkerScript/Utilities.cs
@@ -33,6 +33,16 @@ namespace IngameScript
         }
 
         /// <summary>
+        /// Converts radians to degrees
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static float ToDegrees(this float radians)
+        {
+            return MathHelper.ToDegrees(radians);
+        }
+
+        /// <summary>
         /// Converts degrees to radians
         /// </summary>
         /// <param name="degrees"></param>
@@ -43,12 +53,31 @@ namespace IngameScript
         }
 
         /// <summary>
+        /// Converts degrees to radians
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
+        public static float ToRadians(this float degrees)
+        {
+            return MathHelper.ToRadians(degrees);
+        }
+
+        /// <summary>
         /// Turns a -180 to 180 (since rotors operate between 0 and 360)
         /// </summary>
         /// <param name="degrees"></param>
+        /// <param name="nineties">If it should be from -90 to 90 (hinges)</param>
         /// <returns></returns>
-        public static double AbsoluteDegrees(this double degrees)
+        public static double AbsoluteDegrees(this double degrees, bool nineties = false)
         {
+            if (nineties)
+            {
+                while (degrees < -90)
+                    degrees += 180;
+                while (degrees > 90)
+                    degrees -= 180;
+                return degrees;
+            }
             while (degrees < 0)
                 degrees += 360;
             while (degrees > 360)
