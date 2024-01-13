9de0c29a776930b1a497a7e1a7dbd3040205858c
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2024-01-12 22:03:06 -0600

    Project reorganization

---
+++ AdvancedWalkerScript/LegGroup.cs
@@ -0,0 +1,83 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class LegGroup
+        {
+
+            #region # - Properties
+
+            public static Program Program; // set in the Program's constructor, a reference so we don't need to pass it in the constructor
+            // ^ bad practice but this is a space engineers script, this isn't NASA!
+
+            public LegConfiguration Configuration;
+
+            public IMyMotorStator[] HipStators;
+            public IMyMotorStator[] KneeStators;
+            public IMyMotorStator[] FootStators;
+            public IMyCameraBlock[] InclineCameras;
+
+            public double AnimationStep = 0; // pff, who needes getters and setters?
+            public double Offset = 0;
+            public double OffsetPassed = 0;
+
+            #endregion
+
+            #region # - Constructor
+
+            public LegGroup(IMyMotorStator[] hips, IMyMotorStator[] knees, IMyMotorStator[] feet)
+            {
+                HipStators = hips;
+                KneeStators = knees;
+                FootStators = feet;
+            }
+
+            public LegGroup() {}
+
+            #endregion
+
+            #region # - Methods
+
+            public virtual void Update(double delta)
+            {
+                if (OffsetPassed < Offset)
+                {
+                    OffsetPassed += delta * 2;
+                    if (OffsetPassed >= Offset)
+                        delta = OffsetPassed - Offset; // gets the time over the offset, so if its 9.2 / 9 we get .2 points of delta back
+                    else
+                    {
+                        delta = 0;
+                        AnimationStep = 0;
+                    }
+                }
+
+                AnimationStep += delta * Configuration.AnimationSpeed;
+                AnimationStep %= 4; // 0 to 3
+            }
+
+            #endregion
+
+        }
+    }
+}
---
+++ AdvancedWalkerScript/Legs/ChickenWalkerLegGroup.cs
@@ -0,0 +1,80 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class ChickenWalkerLegGroup : LegGroup
+        {
+            public override void Update(double delta)
+            {
+                base.Update(delta);
+
+                /*double step = AnimationStep; //+ 2 % 4;
+
+                double hipMultiplifer = InvertHips ? -1d : 1d;
+                double kneesMultiplier = InvertKnees ? -1d : 1d;
+                double feetMultiplier = InvertFeet ? -1d : 1d;
+
+                double hipAngleRad = HipStator.Angle;
+                double hipAngleDeg = hipAngleRad.ToDegrees();
+                double kneeAngleRad = KneeStator.Angle;
+                double kneeAngleDeg = kneeAngleRad.ToDegrees();
+                double footAngleRad = FootStator.Angle;
+                double footAngleDeg = footAngleRad.ToDegrees();
+
+                double sin = Math.Sin(MathHelper.ToRadians(step / 8 * 360));
+                double sin2 = Math.Sin(MathHelper.ToRadians(step / 4 * 360));
+                double cos = Math.Cos(MathHelper.ToRadians(step / 4 * 360));
+
+                double hipTargetRad = (
+                    -60 + Math.Sin(step / 4 * Math.PI * 2) * 15
+                ).ToRadians();
+                double kneeTargetRad = hipTargetRad - (
+                    Math.Cos(step / 4 * Math.PI * 2) * 20
+                ).ToRadians() - (30d).ToRadians();
+
+                double hipTargetDeg = (hipTargetRad.ToDegrees() * hipMultiplifer);
+                double kneeTargetDeg = (kneeTargetRad.ToDegrees() * kneesMultiplier * hipMultiplifer);
+                double footTargetDeg = ((kneeAngleDeg - hipAngleDeg) * (double)feetMultiplier);
+
+
+                hipTargetDeg = hipTargetDeg.AbsoluteDegrees(); // for rotors
+                kneeTargetDeg = kneeTargetDeg.AbsoluteDegrees(); // for rotors
+
+                double hipTargetRPM = MathHelper.Clamp(hipTargetDeg - hipAngleDeg - HipOffset - 0, -MaxRPM, MaxRPM);
+                double kneeTargetRPM = MathHelper.Clamp(kneeTargetDeg - kneeAngleDeg - KneeOffset, -MaxRPM, MaxRPM);
+                double footTargetRPM = MathHelper.Clamp(footTargetDeg - footAngleDeg - FootOffset, -MaxRPM, MaxRPM);
+
+                debug?.WriteText($"\n{Math.Sin(MathHelper.ToRadians(AnimationStep / 8 * 360))}", true);
+                debug?.WriteText($"\n{AnimationStep};{step}", true);
+                debug?.WriteText($"\n{step};{hipTargetRPM};{kneeTargetRPM}", true);
+
+                debug?.WriteText($"\n{HipStator.CustomName}: {hipTargetDeg}/{hipAngleDeg}; rotating {hipTargetRPM}", true);
+                debug?.WriteText($"\n{KneeStator.CustomName}: {kneeTargetDeg}/{kneeAngleDeg}; rotating {kneeTargetRPM}", true);
+
+                HipStator.TargetVelocityRPM = (float)hipTargetRPM;
+                KneeStator.TargetVelocityRPM = (float)kneeTargetRPM;
+                FootStator.TargetVelocityRPM = (float)footTargetRPM;*/
+            }
+        }
+    }
+}
