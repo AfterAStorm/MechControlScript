68f88a00f9b4504cccadb858eead5329b204dfda
AfterAStorm <86730204+AfterAStorm@users.noreply.github.com>
2024-01-17 17:13:55 -0600

    Change walk cycle

--- AdvancedWalkerScript/Legs/ChickenWalkerLegGroup.cs
+++ AdvancedWalkerScript/Legs/ChickenWalkerLegGroup.cs
@@ -31,12 +31,52 @@ namespace IngameScript
                 // shamelessly borrowed from https://opentextbooks.clemson.edu/wangrobotics/chapter/inverse-kinematics/
                 // the equations anyway
                 // x and y are flipped, yay!
-                double targetY = Math.Sin((step / 4 * 360).ToRadians()) * 1.5f * Configuration.StepLengthMultiplier - 1; // x  //Math.Sin((AnimationStep / 4 * 360).ToRadians()) * 3;
-                double targetX = (MathHelper.Clamp(3 - Math.Cos((step / 4 * 360).ToRadians()) * 2f, float.MinValue, 2.5f) + 1 - (crouch ? 2 : 0)) * StandingHeight; // y  //Math.Cos((AnimationStep / 4 * 360).ToRadians()) * 3;
-                if (Animation == Animation.Turn)
+
+                // Should cache this, but pff
+                double thighLength = Configuration.ThighLength >= 0 ? Configuration.ThighLength :
+                    (LeftHipStators.Count > 0 && LeftKneeStators.Count > 0 ? Vector3.Distance(LeftHipStators[0].Stator.GetPosition(), LeftKneeStators[0].Stator.GetPosition()) :
+                    (RightHipStators.Count > 0 && RightKneeStators.Count > 0 ? Vector3.Distance(RightHipStators[0].Stator.GetPosition(), RightKneeStators[0].Stator.GetPosition()) : 0));//2.5f;
+                double calfLength = Configuration.CalfLength >= 0 ? Configuration.CalfLength :
+                    (LeftFootStators.Count > 0 && LeftKneeStators.Count > 0 ? Vector3.Distance(LeftFootStators[0].Stator.GetPosition(), LeftKneeStators[0].Stator.GetPosition()) :
+                    (RightFootStators.Count > 0 && RightKneeStators.Count > 0 ? Vector3.Distance(RightFootStators[0].Stator.GetPosition(), RightKneeStators[0].Stator.GetPosition()) : 0));//2.5f;
+
+                double maxDistance = (thighLength + calfLength) * StandingHeight;
+                //Log($"Lengths: {thighLength} {calfLength}");
+
+                // with sin, 0-2 is 0 to 1 to 0, while 2-4 is 0 to -1 to 0
+                //        /\        .     1
+                //       /  \      .
+                // _____/____\____.__     0
+                //     .      \  /
+                //    .        \/        -1
+                //      0  1  2 3 4
+
+                // with cos, 0-2 is 1 to 0 to -1, while 2-4 is -1 to 0 to 1
+                //     .\        /.       1
+                //    .  \      /  .
+                //  _.____\____/___ .     0
+                //  .      \  /      .
+                // .        \/        .  -1
+                //      0 1  2 3 4
+
+                // Instead of converting (step / 4 * 360) to radians, we just multiply the number of radians instead! It's TECHNICALLY more accurate too (the output of Sin/Cos at least)!
+                double stepPercent = step / 4d;
+                double sin = Math.Sin(stepPercent * 2 * Math.PI);
+                double cos = Math.Cos(stepPercent * 2 * Math.PI);
+
+                double x = sin * 1.5d * Configuration.StepLengthMultiplier - 1 + (Animation == Animation.Walk || Animation == Animation.CrouchWalk ? AccelerationLean : StandingLean);
+                double y = MathHelper.Clamp(((maxDistance) - cos * (maxDistance * StandingHeight) + 1 - (crouch ? 2 : 0)), double.MinValue, 4f * StandingHeight);
+
+                Log($"xy: {x}, {y}, {maxDistance}");
+
+                return InverseKinematics.CalculateLeg(thighLength, calfLength, x, y);
+
+                /*double targetY = Math.Sin((stepPercent * 360).ToRadians()) * 1.5f * Configuration.StepLengthMultiplier - 1; // x  //Math.Sin((AnimationStep / 4 * 360).ToRadians()) * 3;
+                double targetX = (MathHelper.Clamp(3 - Math.Cos((stepPercent * 360).ToRadians()) * 2f, float.MinValue, 2.5f) + 1 - (crouch ? 2 : 0)) * StandingHeight; // y  //Math.Cos((AnimationStep / 4 * 360).ToRadians()) * 3;
+                if (Animation == Animation.Turn) // makes math above redudant, but rarely used so it's fine!
                 {
                     targetY = -1;
-                    targetX = MathHelper.Clamp(Math.Sin((step / 4 * 360).ToRadians()), double.MinValue, 0) * 1.5f + 3;
+                    targetX = MathHelper.Clamp(Math.Sin((stepPercent * 360).ToRadians()), double.MinValue, 0) * 1.5f + 3;
                 }
 
                 Log(targetY, targetX);
@@ -44,9 +84,7 @@ namespace IngameScript
                         Math.Pow(targetX - 0, 2) +
                         Math.Pow(targetY - 0, 2)
                 );
-
-                double thighLength = Configuration.ThighLength;//2.5f;
-                double calfLength = Configuration.CalfLength;//2.5f;
+                distance = MathHelper.Clamp(distance, 0, maxDistance);
 
                 double diameter = Math.Atan(targetY / targetX);
                 double leftDiameter = Math.Acos(
@@ -70,7 +108,7 @@ namespace IngameScript
                     HipDegrees = hipAngle.ToDegrees(),
                     KneeDegrees = kneeAngle.ToDegrees(),
                     FeetDegrees = footDeg
-                };
+                };*/
                 /*double hipDeg =
                     (!crouch ? -60 : -70) + Math.Sin(step / 4 * Math.PI * 2) * 15d * (crouch ? .5d : 1d);
                 double kneeDeg =
@@ -121,16 +159,16 @@ namespace IngameScript
                                 lg.AutoLock = false;
                                 lg.Unlock();
                             }
-                            leftAngles = CalculateAngles(AnimationWaitTime + 2f);
+                            leftAngles = CalculateAngles(AnimationStep - .5f);
                             rightAngles = CalculateAngles(AnimationWaitTime + 2f);
                             break;
                         }
-                        else if (absWaitTime <= 2.3f)
+                        /*else if (absWaitTime <= 2.3f)
                         {
                             leftAngles = CalculateAngles(AnimationStep);
                             rightAngles = CalculateAngles(3f * (AnimationWaitTime / Math.Abs(AnimationWaitTime)));
                             break;
-                        }
+                        }*/
                         // else
                         leftAngles = CalculateAngles(AnimationStep);
                         rightAngles = CalculateAngles(AnimationStepOffset);
@@ -161,14 +199,7 @@ namespace IngameScript
                 }
 
                 Log("ChickenWalker (right):", rightAngles.HipDegrees, rightAngles.KneeDegrees, rightAngles.FeetDegrees);
-                SetAngles(
-                    leftAngles.HipDegrees,
-                    leftAngles.KneeDegrees,
-                    leftAngles.FeetDegrees,
-                    rightAngles.HipDegrees,
-                    rightAngles.KneeDegrees,
-                    -rightAngles.FeetDegrees
-                );
+                SetAngles(leftAngles, rightAngles * new LegAngles(1, 1, -1));
             }
         }
     }
